8d8df7af98b1b8aaf57550ba0c6f22bd620fef11
alira <aliraquel95@gmail.com>
2026-04-28 15:10:00 -0600

    m

---
+++ Practica_herencia.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practicas.lab;
+
+/**
+ *
+ * @author alira
+ */
+public class Practica_herencia {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
